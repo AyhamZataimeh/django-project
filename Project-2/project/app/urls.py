fd11f8211f1804db4c02e78b62762cf4d403e822
AyhamZataimeh <ayham.zataimeh99@gmail.com>
2022-01-05 04:03:35 +0200

    push changes

--- Project-2/project/app/urls.py
+++ Project-2/project/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic.base import RedirectView, View
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import permission_required
 from . import views
 from django.conf.urls.static import static
@@ -11,6 +12,14 @@ urlpatterns=[
     path('',RedirectView.as_view(url='home')),
     path('accounts/login/',views.login_view,name='login'),
     path('accounts/logout/',views.logout_view,name='logout'),
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='app/password_reset.html'),name='password-reset'),
+    path('password-reset/done',auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html' ),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+    auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confrim.html' ),name='password_reset_confirm'),
+    path('password-reset-complete/',
+    auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html' )
+    ,name='password_reset_complete'),
+    
     path('signup/',views.signin_view,name='signup'),
     path('profile/',views.profile,name='profile'),
     path('usres/',views.Users.as_view(),name='users'),
